6e7e455cbc1cf5a6fc25a417c52472d30e050a0f
njdiazsoler <njdiaz90@gmail.com>
2020-07-01 21:05:01 -0300

    added api controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,33 @@
+import PokeApiService from '../services/pokeApi';
+
 const initializeRoutes = (app) => {
-  app.use('/', (req, res) => res.send('Hello World!'));
+  app.get('/', (req, res) => res.send('Hello World!'));
+  app.get('/pokemon', async (req, res, next) => {
+    
+    const { limit, offset } = req.params;
+    const options = {
+      limit: limit || 20,
+      offset: offset || 0,
+    }
+    try{
+      const pokemonData = await PokeApiService.getAllPokemon(options.limit, options.offset);
+      return res.sendData(pokemonData);
+    } catch(err){
+      console.error('sale por error!');
+      next(err);
+    }
+  });
+
+  app.get('/pokemon/:name', async (req, res, next) => {
+    const { name } = req.params;
+    try{
+      const pokemonData = await PokeApiService.getOnePokemonByName(name);
+      res.sendData(pokemonData);
+    }catch(err){
+      console.error(err);
+      next(err);
+    }
+  });
 };
 
 export default { initializeRoutes };
